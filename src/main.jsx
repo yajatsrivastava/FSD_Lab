00ac80b55c1277eaa5f095cb5f1bbd4e64009cf1
yajatsrivastava <ys24csb0b85@student.nitw.ac.in>
2026-08-17 23:25:41 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </HashRouter>
   </React.StrictMode>
-);
+)
